38fcd47e03aaae403edecc8650588e47f8bbe1ff
balaji_hublog <hublogfrontend@gmail.com>
2025-05-07 12:38:07 +0530

    getcandidateById query changes

--- models/CandidatesModal.js
+++ models/CandidatesModal.js
@@ -342,7 +342,7 @@ const candidatesModal = {
 
   getCandidatesById: async (candidateId) => {
     try {
-      const query = `SELECT c.*, cr.name AS course_name FROM candidates c INNER JOIN course cr ON cr.id = c.course_id WHERE id = ?`;
+      const query = `SELECT c.*, cr.name AS course_name FROM candidates c INNER JOIN course cr ON cr.id = c.course_id WHERE c.id = ?`;
       const values = [candidateId];
 
       const [result] = await pool.query(query, values);
@@ -368,7 +368,7 @@ const candidatesModal = {
       });
       return formattedResult;
     } catch (error) {
-      throw new Error("Error getting particulat candidate: " + error.message);
+      throw new Error("Error getting particular candidate: " + error.message);
     }
   },
 
